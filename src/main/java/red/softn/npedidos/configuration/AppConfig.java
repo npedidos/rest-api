9f94706ab41bd9ccc4d8e8c0d4a096af3fa302e4
Nicolás Marulanda P <nicomaru@outlook.com>
2022-05-25 21:57:55 +0200

    Clase AppConfig - Se mueve al paquete "configuration".

--- src/main/java/red/softn/npedidos/configuration/AppConfig.java
+++ src/main/java/red/softn/npedidos/configuration/AppConfig.java
@@ -1,4 +1,4 @@
-package red.softn.npedidos;
+package red.softn.npedidos.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
