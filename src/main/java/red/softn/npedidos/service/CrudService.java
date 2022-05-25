c7755cb014e5e033954a6a5185f5dbb3212bb990
Nicolás Marulanda P <nicomaru@outlook.com>
2022-05-26 00:06:04 +0200

    Control de errores en los servicios - Ahora al borrar o actualizar se comprueba que el ID exista. - Ahora la crear un nuevo registro, se establece el campo ID a null.

--- src/main/java/red/softn/npedidos/service/CrudService.java
+++ src/main/java/red/softn/npedidos/service/CrudService.java
@@ -2,10 +2,11 @@ package red.softn.npedidos.service;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.util.ReflectionUtils;
+import red.softn.npedidos.exception.NotFoundException;
+import red.softn.npedidos.pojo.ErrorDescription;
 
 import java.lang.reflect.Field;
 import java.util.List;
-import java.util.function.Supplier;
 
 public abstract class CrudService<T, ID> {
     
@@ -21,27 +22,36 @@ public abstract class CrudService<T, ID> {
     }
     
     public T save(T typeDish) {
+        setFieldId(null, typeDish);
+        
         return getRepository().save(typeDish);
     }
     
     public void delete(ID id) {
+        checkIsExistById(id);
         getRepository().deleteById(id);
     }
     
     public T update(ID id, T typeDish) {
-        String message = "El registro no existe";
-        Supplier<RuntimeException> dishNotFound = () -> new RuntimeException(message);
+        checkIsExistById(id);
+        setFieldId(id, typeDish);
         
-        this.getRepository()
-            .findById(id)
-            .orElseThrow(dishNotFound);
+        return getRepository().save(typeDish);
+    }
+    
+    private void checkIsExistById(ID id) {
+        if (!getRepository().existsById(id)) {
+            throw new NotFoundException(new ErrorDescription("El registro no existe"));
+        }
+    }
+    
+    private void setFieldId(ID id, T typeDish) {
         Field field = ReflectionUtils.findField(typeDish.getClass(), "id");
         
         if (field != null) {
             ReflectionUtils.makeAccessible(field);
             ReflectionUtils.setField(field, typeDish, id);
         }
-        
-        return getRepository().save(typeDish);
     }
+    
 }
