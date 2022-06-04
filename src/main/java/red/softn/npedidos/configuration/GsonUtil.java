8a5ea3beb89c8af4715869a4bbac075e3efbd031
Nicolás Marulanda P <nicomaru@outlook.com>
2022-06-04 17:11:58 +0200

    Clase GsonUtil

---
+++ src/main/java/red/softn/npedidos/configuration/GsonUtil.java
@@ -0,0 +1,31 @@
+package red.softn.npedidos.configuration;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.ResolvableType;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class GsonUtil {
+    
+    private final Gson gson;
+    
+    public <R> R convertTo(Object object, Class<?> clazz) {
+        TypeToken<?> typeToken;
+        boolean isList = ResolvableType.forClass(List.class)
+                                       .isAssignableFrom(ResolvableType.forInstance(object));
+        
+        if (isList) {
+            typeToken = TypeToken.getParameterized(List.class, clazz);
+        } else {
+            typeToken = TypeToken.get(clazz);
+        }
+        
+        return gson.fromJson(gson.toJson(object), typeToken.getType());
+    }
+    
+}
