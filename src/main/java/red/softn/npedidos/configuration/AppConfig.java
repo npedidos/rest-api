7a73141c6679fc5a36d125d6d6c7024a9dc0aef7
Nicolás Marulanda P <nicomaru@outlook.com>
2022-07-23 03:45:56 +0200

    Mostrar errores html - Clase CustomBasicErrorController - Se sobreescribe los errores por defecto de spring de tipo html.

--- src/main/java/red/softn/npedidos/configuration/AppConfig.java
+++ src/main/java/red/softn/npedidos/configuration/AppConfig.java
@@ -1,5 +1,9 @@
 package red.softn.npedidos.configuration;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.web.ErrorProperties;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.method.HandlerTypePredicate;
@@ -7,11 +11,19 @@ import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class AppConfig implements WebMvcConfigurer {
+    
+    private final ServerProperties serverProperties;
     
     @Override
     public void configurePathMatch(PathMatchConfigurer configurer) {
         configurer.addPathPrefix("/api", HandlerTypePredicate.forAnnotation(RestController.class));
+    }
+    
+    @Bean
+    public ErrorProperties errorProperties() {
+        return this.serverProperties.getError();
     }
     
 }
---
+++ src/main/java/red/softn/npedidos/controller/spring/CustomBasicErrorController.java
@@ -0,0 +1,95 @@
+package red.softn.npedidos.controller.spring;
+
+import lombok.Getter;
+import org.springframework.boot.autoconfigure.web.ErrorProperties;
+import org.springframework.boot.autoconfigure.web.servlet.error.AbstractErrorController;
+import org.springframework.boot.autoconfigure.web.servlet.error.BasicErrorController;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorViewResolver;
+import org.springframework.boot.web.error.ErrorAttributeOptions;
+import org.springframework.boot.web.servlet.error.ErrorAttributes;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Ver {@link BasicErrorController}
+ */
+@Controller
+@RequestMapping("/error")
+public class CustomBasicErrorController extends AbstractErrorController {
+    
+    @Getter
+    private final ErrorProperties errorProperties;
+    
+    public CustomBasicErrorController(ErrorAttributes errorAttributes, ErrorProperties errorProperties, List<ErrorViewResolver> errorViewResolvers) {
+        super(errorAttributes, errorViewResolvers);
+        Assert.notNull(errorProperties, "ErrorProperties must not be null");
+        this.errorProperties = errorProperties;
+    }
+    
+    @RequestMapping
+    public ResponseEntity<Map<String, Object>> error(HttpServletRequest request) {
+        HttpStatus status = getStatus(request);
+        
+        if (status == HttpStatus.NO_CONTENT) {
+            return new ResponseEntity<>(status);
+        }
+        
+        Map<String, Object> body = getErrorAttributes(request, getErrorAttributeOptions(request));
+        
+        return new ResponseEntity<>(body, status);
+    }
+    
+    protected ErrorAttributeOptions getErrorAttributeOptions(HttpServletRequest request) {
+        ErrorAttributeOptions options = ErrorAttributeOptions.defaults();
+        
+        if (this.errorProperties.isIncludeException()) {
+            options = options.including(ErrorAttributeOptions.Include.EXCEPTION);
+        }
+        
+        if (isIncludeStackTrace(request)) {
+            options = options.including(ErrorAttributeOptions.Include.STACK_TRACE);
+        }
+        
+        if (isIncludeMessage(request)) {
+            options = options.including(ErrorAttributeOptions.Include.MESSAGE);
+        }
+        
+        if (isIncludeBindingErrors(request)) {
+            options = options.including(ErrorAttributeOptions.Include.BINDING_ERRORS);
+        }
+        
+        return options;
+    }
+    
+    protected boolean isIncludeMessage(HttpServletRequest request) {
+        return switch (getErrorProperties().getIncludeMessage()) {
+            case ALWAYS -> true;
+            case ON_PARAM -> getMessageParameter(request);
+            default -> false;
+        };
+    }
+    
+    protected boolean isIncludeBindingErrors(HttpServletRequest request) {
+        return switch (getErrorProperties().getIncludeBindingErrors()) {
+            case ALWAYS -> true;
+            case ON_PARAM -> getErrorsParameter(request);
+            default -> false;
+        };
+    }
+    
+    protected boolean isIncludeStackTrace(HttpServletRequest request) {
+        return switch (getErrorProperties().getIncludeStacktrace()) {
+            case ALWAYS -> true;
+            case ON_PARAM -> getTraceParameter(request);
+            default -> false;
+        };
+    }
+    
+}
