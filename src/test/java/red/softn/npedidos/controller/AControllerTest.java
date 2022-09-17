8638babcd37cc45ff806fd9064c3c71020a57bd5
Nicolás Marulanda P <nicomaru@outlook.com>
2022-09-17 20:48:31 +0200

    Clase AControllerTest, como clase común de los test de controladores

---
+++ src/test/java/red/softn/npedidos/controller/AControllerTest.java
@@ -0,0 +1,31 @@
+package red.softn.npedidos.controller;
+
+import com.google.gson.Gson;
+import lombok.Getter;
+import net.datafaker.Faker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.web.servlet.MockMvc;
+import red.softn.npedidos.utils.gson.GsonConfig;
+import red.softn.npedidos.utils.gson.GsonUtil;
+
+@Import({GsonConfig.class})
+@Getter
+public abstract class AControllerTest {
+    
+    @MockBean
+    private GsonUtil gsonUtil;
+    
+    @Autowired
+    private Faker faker;
+    
+    @Autowired
+    private MockMvc mockMvc;
+    
+    @Autowired
+    private Gson gson;
+    
+    public abstract String getUrlMapping();
+    
+}
