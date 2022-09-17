7feb3981b6ed6d27da65b983ea44500dc6c08845
Nicolás Marulanda P <nicomaru@outlook.com>
2022-09-17 20:51:34 +0200

    Se elimina la anotación "Import" de la clase - Ahora esta anotación es usada en la clase AControllerTest.

--- src/test/java/red/softn/npedidos/controller/ExcludeDayControllerTest.java
+++ src/test/java/red/softn/npedidos/controller/ExcludeDayControllerTest.java
@@ -3,14 +3,11 @@ package red.softn.npedidos.controller;
 import lombok.Getter;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Import;
 import red.softn.npedidos.request.ExcludeDayRequest;
 import red.softn.npedidos.response.ExcludeDayResponse;
 import red.softn.npedidos.service.ExcludeDayService;
-import red.softn.npedidos.utils.gson.GsonConfig;
 
 @WebMvcTest(ExcludeDayController.class)
-@Import({GsonConfig.class})
 @Getter
 class ExcludeDayControllerTest extends CrudControllerTest<ExcludeDayRequest, ExcludeDayResponse, Integer, ExcludeDayControllerTestUtil> {
     
