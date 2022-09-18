8fed542bf08d36d89cda35359a14c4d8f0c684eb
Nicolás Marulanda P <nicomaru@outlook.com>
2022-09-18 02:41:30 +0200

    Incorporando Swagger - Se agrega la librería de "springdoc-openapi". - Se modifican algunas etiquetas del fichero POM, agregando información del proyecto. - Se crean nuevas propiedades en el fichero "application.properties". - Se configura de forma básica un Bean, en la clase AppConfig, de OpenAPI.

--- src/main/java/red/softn/npedidos/configuration/AppConfig.java
+++ src/main/java/red/softn/npedidos/configuration/AppConfig.java
@@ -1,5 +1,8 @@
 package red.softn.npedidos.configuration;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import lombok.RequiredArgsConstructor;
 import net.datafaker.Faker;
 import org.springframework.beans.factory.annotation.Value;
@@ -21,6 +24,21 @@ public class AppConfig implements WebMvcConfigurer {
     @Value("${app.http.path.prefix:}")
     private String pathPrefix;
     
+    @Value("${app.name:}")
+    private String appName;
+    
+    @Value("${app.description:}")
+    private String appDescription;
+    
+    @Value("${app.version:0}")
+    private String appVersion;
+    
+    @Value("${app.licenses.name:}")
+    private String appLicensesName;
+    
+    @Value("${app.licenses.url:}")
+    private String appLicensesUrl;
+    
     @Override
     public void configurePathMatch(PathMatchConfigurer configurer) {
         configurer.addPathPrefix(pathPrefix, HandlerTypePredicate.forAnnotation(RestController.class));
@@ -34,6 +52,18 @@ public class AppConfig implements WebMvcConfigurer {
     @Bean
     public Faker faker() {
         return new Faker();
+    }
+    
+    @Bean
+    public OpenAPI openAPI() {
+        var license = new License().name(appLicensesName)
+                                   .url(appLicensesUrl);
+        var info = new Info().title(appName)
+                             .description(appDescription)
+                             .version(appVersion)
+                             .license(license);
+        
+        return new OpenAPI().info(info);
     }
     
 }
