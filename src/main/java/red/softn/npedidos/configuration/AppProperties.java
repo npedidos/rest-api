e7ee3e0b6892d8ad0338ec724e6a7af192c09539
Juangui Restrepo <juangui.rp@gmail.com>
2022-10-04 06:04:44 -0500

    🌟Added a new configuration property file to allow quick access to application properties inside code.

---
+++ src/main/java/red/softn/npedidos/configuration/AppProperties.java
@@ -0,0 +1,35 @@
+package red.softn.npedidos.configuration;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Getter
+public class AppProperties {
+    
+    @Value("${app.http.path.prefix}")
+    private String pathPrefix;
+    
+    @Value("${app.security.jwt.secret}")
+    private String secretKey;
+    
+    @Value("${app.name}")
+    private String appName;
+    
+    @Value("${app.description}")
+    private String appDescription;
+    
+    @Value("${app.version:0}")
+    private String appVersion;
+    
+    @Value("${app.licenses.name}")
+    private String appLicensesName;
+    
+    @Value("${app.licenses.url:}")
+    private String appLicensesUrl;
+    
+    @Value("${app.http.paths.permit-all:}")
+    private String[] permitAllPaths;
+    
+}
