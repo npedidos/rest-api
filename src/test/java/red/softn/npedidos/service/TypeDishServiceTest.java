d15e10f727106bea1315ffaed01ead4a114ffdbd
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2022-10-09 18:21:22 +0200

    Optimize imports 😆

--- src/test/java/red/softn/npedidos/service/TypeDishServiceTest.java
+++ src/test/java/red/softn/npedidos/service/TypeDishServiceTest.java
@@ -1,19 +1,15 @@
 package red.softn.npedidos.service;
 
 import lombok.Getter;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import red.softn.npedidos.CrudServiceTest;
-import red.softn.npedidos.entity.FoodDish;
 import red.softn.npedidos.entity.TypeDish;
 import red.softn.npedidos.repository.TypeDishRepository;
 import red.softn.npedidos.request.TypeDishRequest;
 import red.softn.npedidos.response.TypeDishResponse;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * @author <a href="https://github.com/anas-elgarhy">Anas Elgarhy</a>
