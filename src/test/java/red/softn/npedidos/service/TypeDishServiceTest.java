abce915d5f7fa79a117de62f1f7672a9975c3d67
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2022-10-09 18:15:54 +0200

    Create TypeDishService unit test 💙

---
+++ src/test/java/red/softn/npedidos/service/TypeDishServiceTest.java
@@ -0,0 +1,33 @@
+package red.softn.npedidos.service;
+
+import lombok.Getter;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import red.softn.npedidos.CrudServiceTest;
+import red.softn.npedidos.entity.FoodDish;
+import red.softn.npedidos.entity.TypeDish;
+import red.softn.npedidos.repository.TypeDishRepository;
+import red.softn.npedidos.request.TypeDishRequest;
+import red.softn.npedidos.response.TypeDishResponse;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author <a href="https://github.com/anas-elgarhy">Anas Elgarhy</a>
+ * @version 1.0
+ * @since 09/10/2022
+ */
+@ExtendWith(MockitoExtension.class)
+@Getter
+class TypeDishServiceTest extends CrudServiceTest<TypeDishRequest, TypeDishResponse, TypeDish, Integer, TypeDishServiceTestUtil> {
+    
+    @InjectMocks
+    private TypeDishService service;
+    
+    @Mock
+    private TypeDishRepository repository;
+    
+}
