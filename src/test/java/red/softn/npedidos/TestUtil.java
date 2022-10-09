dc945bb50e993fb0ad72aa5e545533654a756aeb
Loic Beylot <loic.beylot@gmail.com>
2022-10-09 12:01:59 +0100

    Use Faker object to generate email address and password

--- src/test/java/red/softn/npedidos/TestUtil.java
+++ src/test/java/red/softn/npedidos/TestUtil.java
@@ -39,7 +39,11 @@ public abstract class TestUtil {
     }
     
     public String fakeRandomEmail() {
-        return this.fakeRandomWord() + "@" + this.fakeRandomWord() + ".com";
+        return this.faker.internet().emailAddress();
+    }
+    
+    public String fakeRandomPassword() {
+        return this.faker.internet().password(60, 60);
     }
     
 }
