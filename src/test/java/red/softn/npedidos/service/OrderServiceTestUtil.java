52dc062930877ceba5a9f6ab3b10a2013646f31b
Jesús Núñez Contreras <jesusnunez9111@hotmail.com>
2022-10-10 11:46:56 -0500

    Add unit tests for the order service

---
+++ src/test/java/red/softn/npedidos/service/OrderServiceTestUtil.java
@@ -0,0 +1,40 @@
+package red.softn.npedidos.service;
+
+import net.datafaker.Faker;
+import red.softn.npedidos.CrudServiceTestUtil;
+import red.softn.npedidos.entity.FoodDish;
+import red.softn.npedidos.entity.Order;
+import red.softn.npedidos.request.FoodDishRequest;
+import red.softn.npedidos.request.OrderRequest;
+import red.softn.npedidos.response.FoodDishResponse;
+import red.softn.npedidos.response.OrderResponse;
+
+import java.time.LocalDateTime;
+
+public class OrderServiceTestUtil extends CrudServiceTestUtil<OrderRequest, OrderResponse, Order, Integer> {
+    
+    public OrderServiceTestUtil(Faker faker) {
+        super(faker);
+    }
+    
+    @Override
+    public Init<OrderRequest, OrderResponse, Order, Integer> init() {
+    
+        var request = new OrderRequest();
+        var response = new OrderResponse();
+        var entity = new Order();
+        var entitySaveResult = new Order();
+    
+        entity.setId(fakeRandomInteger());
+        entity.setDateOrder(LocalDateTime.of(2022, 10, 10, 10, 10));
+        entitySaveResult.setId(entity.getId());
+        entitySaveResult.setDateOrder(entity.getDateOrder());
+        response.setId(entity.getId());
+        response.setDateOrder(entity.getDateOrder());
+        request.setId(entity.getId());
+        request.setDateOrder(entity.getDateOrder());
+    
+        return Init.of(request, response, entity, entitySaveResult, entity.getId());
+    }
+    
+}
