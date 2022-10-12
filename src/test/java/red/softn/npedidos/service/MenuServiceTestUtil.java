37092de19404a81b2782bbe5eb48be6816050e1a
Medhavi-16 <medhavi.srivastava16@gmail.com>
2022-10-13 03:48:03 +0530

    MenuServiceTest added
    MenuServiceTest added

---
+++ src/test/java/red/softn/npedidos/service/MenuServiceTestUtil.java
@@ -0,0 +1,33 @@
+package red.softn.npedidos.service;
+
+import net.datafaker.Faker;
+import red.softn.npedidos.CrudServiceTestUtil;
+import red.softn.npedidos.entity.Menu;
+import red.softn.npedidos.request.MenuRequest;
+import red.softn.npedidos.response.MenuResponse;
+
+class MenuServiceTestUtil extends CrudServiceTestUtil<MenuRequest, MenuResponse, Menu, Integer> {
+    
+    public MenuServiceTestUtil(Faker faker) {
+        super(faker);
+    }
+    
+    @Override
+    public Init<MenuRequest, MenuResponse, Menu, Integer> init() {
+        var request = new MenuRequest();
+        var response = new MenuResponse();
+        var entity = new Menu();
+        var entitySaveResult = new Menu();
+    
+        entity.setId(fakeRandomInteger());
+        entity.setDate(fakeDateFutureDays());
+        entitySaveResult.setId(entity.getId());
+        entitySaveResult.setDate(entity.getDate());
+        response.setId(entity.getId());
+        response.setDate(entity.getDate());
+        request.setId(entity.getId());
+        request.setDate(entity.getDate());
+        
+        return Init.of(request, response, entity, entitySaveResult, entity.getId());
+    }
+}
