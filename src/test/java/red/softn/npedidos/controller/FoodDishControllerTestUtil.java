0ce99ed79df555a3f2bcd9b936934c55847483c8
Loic Beylot <loic.beylot@gmail.com>
2022-10-18 06:39:30 +0100

    Add unit test for FoodDishController class

---
+++ src/test/java/red/softn/npedidos/controller/FoodDishControllerTestUtil.java
@@ -0,0 +1,28 @@
+package red.softn.npedidos.controller;
+
+import com.google.gson.Gson;
+import red.softn.npedidos.ControllerTestUtil;
+import red.softn.npedidos.TestUtil;
+import red.softn.npedidos.request.FoodDishRequest;
+import red.softn.npedidos.response.FoodDishResponse;
+
+public class FoodDishControllerTestUtil extends ControllerTestUtil<FoodDishRequest, FoodDishResponse, Integer> {
+    
+    public FoodDishControllerTestUtil(Gson gson) {
+        super(gson);
+    }
+    
+    @Override
+    protected Init<FoodDishRequest, FoodDishResponse, Integer> init() {
+        var request = new FoodDishRequest();
+        var response = new FoodDishResponse();
+        
+        request.setId(TestUtil.fakeRandomInteger());
+        request.setName(TestUtil.fakeRandomWord());
+        response.setId(request.getId());
+        response.setName(request.getName());
+        
+        return Init.of(request, response, request.getId());
+    }
+    
+}
