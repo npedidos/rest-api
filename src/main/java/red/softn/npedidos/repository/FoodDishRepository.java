fc7fc71577c372815070fd0ecdf46bf76a9f6e66
Nicolás Marulanda P <nicomaru@outlook.com>
2022-10-22 18:53:47 +0200

    Modificado: Clase FoodDishService - Ahora la búsqueda de menus se retorna paginada.

--- src/main/java/red/softn/npedidos/repository/FoodDishRepository.java
+++ src/main/java/red/softn/npedidos/repository/FoodDishRepository.java
@@ -1,17 +1,17 @@
 package red.softn.npedidos.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import red.softn.npedidos.entity.FoodDish;
 
-import java.util.List;
-
 @Repository
 public interface FoodDishRepository extends JpaRepository<FoodDish, Integer> {
     
     @Query("SELECT f FROM FoodDish f WHERE f.typeDish.id = :id")
-    List<FoodDish> findAllByTypeDishId(Integer id);
+    Page<FoodDish> findAllByTypeDishId(Integer id, Pageable pageable);
     
     @Query("SELECT f FROM FoodDish f WHERE f.id = :id AND f.typeDish.id = :typeDishId")
     FoodDish findByIdAndTypeDishId(Integer id, Integer typeDishId);
--- src/main/java/red/softn/npedidos/service/FoodDishService.java
+++ src/main/java/red/softn/npedidos/service/FoodDishService.java
@@ -2,6 +2,7 @@ package red.softn.npedidos.service;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import red.softn.npedidos.entity.FoodDish;
 import red.softn.npedidos.entity.Menu;
@@ -10,9 +11,8 @@ import red.softn.npedidos.repository.MenuRepository;
 import red.softn.npedidos.request.FoodDishRequest;
 import red.softn.npedidos.response.FoodDishResponse;
 import red.softn.npedidos.response.MenuResponse;
+import red.softn.npedidos.response.PagingAndSortingResponse;
 import red.softn.npedidos.specifications.MenuSpecifications;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -23,11 +23,11 @@ public class FoodDishService extends CrudService<FoodDishRequest, FoodDishRespon
     
     private final MenuRepository menuRepository;
     
-    public List<MenuResponse> findAllMenus(Integer id) {
+    public PagingAndSortingResponse<MenuResponse> findAllMenus(Integer id) {
         //SELECT * FROM menus WHERE ID IN (SELECT menus_id FROM menus_has_food_dishes WHERE food_dishes_id = :id
-        List<Menu> menus = this.menuRepository.findAll(MenuSpecifications.hasFoodDish(id));
+        Page<Menu> menus = this.menuRepository.findAll(MenuSpecifications.hasFoodDish(id), getDataRequestScope().getPageable());
         
-        return getGsonUtil().convertTo(menus, MenuResponse.class);
+        return pageToResponse(menus, MenuResponse.class);
     }
     
 }
