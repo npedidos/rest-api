f5c7a611b401aeb7469b4718fd22a4c14cc832e3
Loic Beylot <quenrelia+misc@gmail.com>
2022-10-26 21:42:23 +0100

    Implementation of unit test for SettingController

---
+++ src/test/java/red/softn/npedidos/controller/SettingControllerTestUtil.java
@@ -0,0 +1,33 @@
+package red.softn.npedidos.controller;
+
+import com.google.gson.Gson;
+import red.softn.npedidos.ControllerTestUtil;
+import red.softn.npedidos.TestUtil;
+import red.softn.npedidos.request.SettingRequest;
+import red.softn.npedidos.response.SettingResponse;
+
+public class SettingControllerTestUtil extends ControllerTestUtil<SettingRequest, SettingResponse, Integer> {
+    
+    public SettingControllerTestUtil(Gson gson) {
+        super(gson);
+    }
+    
+    @Override
+    protected Init<SettingRequest, SettingResponse, Integer> init() {
+        var request = new SettingRequest();
+        var response = new SettingResponse();
+        
+        request.setId(TestUtil.fakeRandomInteger());
+        request.setDescription(TestUtil.fakeRandomWord());
+        request.setValue(TestUtil.fakeRandomWord());
+        request.setKeyName(TestUtil.fakeRandomWord());
+        response.setId(request.getId());
+        response.setDescription(request.getDescription());
+        response.setValue(request.getValue());
+        response.setKeyName(request.getKeyName());
+        
+        
+        return Init.of(request, response, request.getId());
+    }
+    
+}
