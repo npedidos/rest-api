11af498fafca0d34110efbb4ed7df41a5199405b
Nicolás Marulanda P <nicomaru@outlook.com>
2022-10-28 23:51:50 +0200

    Nuevo: API [GET] /orders/{id}/food-dishes - Obtiene los platos de comida de un pedido.

--- src/main/java/red/softn/npedidos/controller/OrderController.java
+++ src/main/java/red/softn/npedidos/controller/OrderController.java
@@ -2,6 +2,9 @@ package red.softn.npedidos.controller;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import red.softn.npedidos.request.OrderRequest;
@@ -15,5 +18,10 @@ import red.softn.npedidos.service.OrderService;
 public class OrderController extends CrudController<OrderRequest, OrderResponse, Integer> {
     
     private final OrderService service;
+    
+    @GetMapping("/{id}/food-dishes")
+    public ResponseEntity<?> findAllFoodDishes(@PathVariable Integer id) {
+        return ResponseEntity.ok(getService().findAllFoodDishes(id));
+    }
     
 }
--- src/main/java/red/softn/npedidos/service/OrderService.java
+++ src/main/java/red/softn/npedidos/service/OrderService.java
@@ -2,11 +2,17 @@ package red.softn.npedidos.service;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
+import red.softn.npedidos.entity.FoodDish;
 import red.softn.npedidos.entity.Order;
+import red.softn.npedidos.repository.FoodDishRepository;
 import red.softn.npedidos.repository.OrderRepository;
 import red.softn.npedidos.request.OrderRequest;
+import red.softn.npedidos.response.FoodDishResponse;
 import red.softn.npedidos.response.OrderResponse;
+import red.softn.npedidos.response.PagingAndSortingResponse;
+import red.softn.npedidos.specifications.OrderFoodDishSpecifications;
 
 @Service
 @RequiredArgsConstructor
@@ -14,5 +20,13 @@ import red.softn.npedidos.response.OrderResponse;
 public class OrderService extends CrudService<OrderRequest, OrderResponse, Order, Integer> {
     
     private final OrderRepository repository;
+    
+    private final FoodDishRepository foodDishRepository;
+    
+    public PagingAndSortingResponse<FoodDishResponse> findAllFoodDishes(Integer id) {
+        Page<FoodDish> foodDishes = this.foodDishRepository.findAll(OrderFoodDishSpecifications.hasFoodDish(id), getDataRequestScope().getPageable());
+        
+        return pageToResponse(foodDishes, FoodDishResponse.class);
+    }
     
 }
