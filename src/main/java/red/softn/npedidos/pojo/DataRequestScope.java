7b0d8604e7ca975fe5c7fb73b4172294a7e6759c
Nicolás Marulanda P <nicomaru@outlook.com>
2023-04-09 01:46:48 +0200

    Nuevo: Método `DataRequestScope.getDefaultLocale()` - Obtiene la instancia de `Locale` determinada por la petición HTTP.

--- src/main/java/red/softn/npedidos/pojo/DataRequestScope.java
+++ src/main/java/red/softn/npedidos/pojo/DataRequestScope.java
@@ -7,8 +7,12 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.RequestScope;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 import red.softn.npedidos.configuration.AppProperties;
 import red.softn.npedidos.request.PagingAndSortingRequest;
+
+import java.util.Locale;
 
 @RequestScope
 @Component
@@ -23,12 +27,18 @@ public class DataRequestScope {
     
     private final AppProperties appProperties;
     
+    private final LocaleResolver localeResolver;
+    
     public Pageable getPageable() {
         if (this.pageable == null) {
             this.pageable = PageRequest.of(0, this.appProperties.getPageableSize());
         }
         
         return pageable;
+    }
+    
+    public Locale getDefaultLocale() {
+        return ((AcceptHeaderLocaleResolver) this.localeResolver).getDefaultLocale();
     }
     
 }
